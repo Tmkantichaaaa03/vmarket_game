7f509c9776969370c680658e66019160d623b858
Tmkantichaaaa03 <momonut6627@gmail.com>
2026-01-28 21:40:04 +0700

    .

--- vmarket_game/shipping_address.gd
+++ vmarket_game/shipping_address.gd
@@ -1,14 +1,13 @@
 extends Control
 
-# ฟังก์ชันนี้จะทำงานเมื่อเข้าสู่ฉากครั้งแรก (ถ้าไม่ได้ใช้ ลบออกได้ครับ)
 func _ready() -> void:
 	pass 
 
-# ฟังก์ชันนี้จะทำงานทุกๆ เฟรม (ถ้าไม่ได้ใช้ ลบออกได้เพื่อประหยัดทรัพยากร)
 func _process(delta: float) -> void:
 	pass
 
-# ฟังก์ชันที่เชื่อมกับปุ่ม "กลับ"
 func _on_texture_button_pressed() -> void:
-	# คำสั่งเปลี่ยนฉากไปยังหน้า house_inside
-	get_tree().change_scene_to_file("res://vmarket_game/shop_scene.tscn")
+	get_tree().change_scene_to_file("res://vmarket_game/shop.tscn")
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://vmarket_game/thakyou.tscn")
