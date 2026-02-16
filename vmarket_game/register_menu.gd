0faea01de128ac0239d93f11700422c557463373
Tmkantichaaaa03 <momonut6627@gmail.com>
2026-02-16 15:24:15 +0700

    .

--- vmarket_game/register_menu.gd
+++ vmarket_game/register_menu.gd
@@ -1,35 +1,29 @@
 extends Control
 
-# รายชื่อตัวละครให้ตรงกับ SpriteFrames
 var characters = ["Adam", "Alex", "Amelia", "Bob"]
 var current_index = 0
 
-# อ้างอิงโหนด (ตรวจสอบชื่อให้ตรงกับ Scene Tree ของคุณ)
 @onready var sprite = $AnimatedSprite2D 
 @onready var http_request = $HTTPRequest
 @onready var alert_dialog = $AlertDialog
-@onready var back_button = $back # อ้างอิงปุ่มย้อนกลับ
+@onready var back_button = $back 
+@onready var register_button = $TextureButton
 
 func _ready():
-	# ตั้งค่าภาพให้คมชัดสไตล์ Pixel Art
 	sprite.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST 
 	update_character()
 	
-	# เชื่อมต่อสัญญาณจากปุ่มต่างๆ
-	$right.pressed.connect(_on_right_pressed)
-	$lift.pressed.connect(_on_lift_pressed)
-	$TextureButton.pressed.connect(_on_register_pressed)
-	back_button.pressed.connect(_on_back_pressed) # เชื่อมต่อปุ่มย้อนกลับ
+	# เชื่อมต่อสัญญาณปุ่ม
+	if $right: $right.pressed.connect(_on_right_pressed)
+	if $lift: $lift.pressed.connect(_on_lift_pressed)
+	if register_button: register_button.pressed.connect(_on_register_pressed)
+	if back_button: back_button.pressed.connect(_on_back_pressed)
 	
-	# เชื่อมต่อสัญญาณ HTTP
 	http_request.request_completed.connect(_on_http_request_completed)
 
-# --- ระบบปุ่มย้อนกลับ ---
 func _on_back_pressed():
-	# เปลี่ยนกลับไปหน้า Login (ตรวจสอบ Path ให้ถูกต้อง)
 	get_tree().change_scene_to_file("res://vmarket_game/login_menu.tscn")
 
-# --- ระบบสลับตัวละคร ---
 func _on_right_pressed():
 	current_index = (current_index + 1) % characters.size()
 	update_character()
@@ -41,51 +35,59 @@ func _on_lift_pressed():
 func update_character():
 	sprite.play(characters[current_index])
 
-# --- ระบบส่งข้อมูลสมัครสมาชิก ---
 func _on_register_pressed():
-	var password = $Password/LineEdit.text
-	var conf_pass = $Confpass/LineEdit.text
+	var username = $Username/LineEdit.text.strip_edges()
+	var password = $Password/LineEdit.text.strip_edges()
+	var conf_pass = $Confpass/LineEdit.text.strip_edges()
 	
+	if username == "" or password == "":
+		show_alert("กรุณากรอกข้อมูลให้ครบ", false)
+		return
+		
 	if password != conf_pass:
 		show_alert("รหัสผ่านไม่ตรงกัน!", false)
 		return
 
-	# เตรียมข้อมูล JSON ให้ตรงกับ PHP
 	var register_data = {
-		"username": $Username/LineEdit.text,
-		"name": $name/LineEdit.text,
-		"email": $email/LineEdit.text,
-		"address": $address/LineEdit.text,
-		"phone_number": $phone/LineEdit.text,
+		"username": username,
+		"name": $name/LineEdit.text.strip_edges(),
+		"email": $email/LineEdit.text.strip_edges(),
+		"address": $address/LineEdit.text.strip_edges(),
+		"phone_number": $phone/LineEdit.text.strip_edges(),
 		"password": password,
-		"avatar_id": current_index + 1 # บันทึกตัวละครที่เลือก
+		"avatar_id": current_index + 1
 	}
 	
 	var json_query = JSON.stringify(register_data)
 	var headers = ["Content-Type: application/json"]
 	var url = "http://localhost/VMARKET/api/auth/register.php"
+	
+	if register_button: register_button.disabled = true
 	http_request.request(url, headers, HTTPClient.METHOD_POST, json_query)
 
-# --- ระบบจัดการผลตอบรับจาก Server ---
 func _on_http_request_completed(_result, response_code, _headers, body):
-	var response_text = body.get_string_from_utf8()
-	var json = JSON.new()
-	var parse_result = json.parse(response_text)
+	if register_button: register_button.disabled = false
 	
-	if parse_result == OK:
-		var response = json.get_data()
-		if response_code == 200: # สมัครสำเร็จ
-			print("สำเร็จ: กำลังเข้าสู่หน้า World...")
-			get_tree().change_scene_to_file("res://vmarket_game/world.tscn") 
+	var response_text = body.get_string_from_utf8().strip_edges()
+	var response = JSON.parse_string(response_text)
+	
+	# แก้ไขให้รองรับ Code 201 Created ตามรูปภาพ
+	if (response_code == 200 or response_code == 201) and response != null:
+		if response.get("success") == true:
+			if response.has("user_data"):
+				Global.current_username = response["user_data"].get("username")
+				Global.selected_avatar_index = int(response["user_data"].get("avatar_id"))
+			
+			# เปลี่ยนไปหน้าหลัก startworld.tscn
+			get_tree().change_scene_to_file("res://vmarket_game/startworld.tscn")
 		else:
-			show_alert("ล้มเหลว: " + response.get("message", "เกิดข้อผิดพลาด"), false)
+			show_alert(response.get("message", "สมัครสมาชิกไม่สำเร็จ"), false)
 	else:
-		show_alert("Error: เซิร์ฟเวอร์ตอบกลับไม่ถูกต้อง", false)
+		show_alert("Server Error (Code: %d)" % response_code, false)
 
-# ฟังก์ชันแสดง Alert
 func show_alert(text: String, _is_success: bool):
 	if alert_dialog:
 		alert_dialog.dialog_text = text
 		alert_dialog.popup_centered()
 	else:
-		print("Alert: ", text)
+		print("แจ้งเตือน: ", text)
